f9af8ba3e35daf39cbf35188d2f8f57c2f10486b
yaroslav <usenko.0265@gmail.com>
2023-05-31 22:26:01 +0300

    fix bugs

--- src/components/Pages/GeneralTickets/GeneralTickets.jsx
+++ src/components/Pages/GeneralTickets/GeneralTickets.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { Button, Grid, Typography } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import { Ticket } from "../../Ticket/Ticket";
-import { useGetAnonTicketsMutation } from "../../../store/api/tickets/tickets.api";
+import { useGetTicketsMutation } from "../../../store/api/tickets/tickets.api";
 import { useJwtDecode } from "../../../shared/hooks";
 import { Loader } from "../../Loader";
 
@@ -11,14 +11,15 @@ const GeneralTickets = () => {
   const [ticketsPerRow, setTicketsPerRow] = useState(2);
   const [tickets, setTickets] = useState([]);
   const jwt = useJwtDecode();
-  const [getAnonTickets, { isLoading, isSuccess }] =
-    useGetAnonTicketsMutation();
+  const [geTickets, result] = useGetTicketsMutation();
+
+  const option = jwt ? "tickets" : "anon";
 
   useEffect(() => {
-    getAnonTickets({ body: JSON.stringify({}) }).then(res => {
+    geTickets({ option: option, body: JSON.stringify({}) }).then(res => {
       setTickets(res.data.ticket_list);
     });
-  }, []);
+  }, [option]);
 
   return (
     <Grid container>
@@ -38,18 +39,21 @@ const GeneralTickets = () => {
         3
       </Button>
       <Grid container gap={2}>
-        {isLoading && <Loader />}
-        {isSuccess &&
-          [...tickets].reverse().map(ticket => {
-            return (
-              <Ticket
-                ticketsPerRow={ticketsPerRow}
-                ticket={ticket}
-                isAuth={!!jwt}
-                key={ticket.ticket_id}
-              />
-            );
-          })}
+        {result.isLoading && <Loader />}
+        {result.isSuccess &&
+          [...tickets]
+            .reverse()
+            .slice(0, 9)
+            .map(ticket => {
+              return (
+                <Ticket
+                  ticketsPerRow={ticketsPerRow}
+                  ticket={ticket}
+                  isAuth={!!jwt}
+                  key={ticket.ticket_id}
+                />
+              );
+            })}
       </Grid>
     </Grid>
   );
--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,6 +1,7 @@
 const endpoints = {
   baseUrl: import.meta.env.VITE_API_URL,
   base: "/",
+  fullTicket: "/tickets",
   dashboard: "/dashboard",
   sent: "/sent",
   received: "/received",
