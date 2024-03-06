cb61b0e13ccd38552fb1ef1b0df550b97529bd99
yaroslav <usenko.0265@gmail.com>
2024-03-06 16:49:02 +0200

    Fix creator_id bug

--- src/pages/FullTicketInfo/FullTicketInfo.tsx
+++ src/pages/FullTicketInfo/FullTicketInfo.tsx
@@ -1,78 +1,78 @@
-import { useEffect, FC, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useEffect, FC, useState } from 'react'
+import { useLocation } from 'react-router-dom'
 
-import Grid from "@mui/material/Grid";
-import useTheme from "@mui/material/styles/useTheme";
+import Grid from '@mui/material/Grid'
+import useTheme from '@mui/material/styles/useTheme'
 
-import { Loader } from "../../components/Loader";
-import { ActionPanel } from "./components/ActionPanel";
-import { MarkdownWithStyles } from "../../utils/markdown";
-import { FullTicketHeader } from "./components/FullTicketHeader";
-import { FullTicketComments } from "./components/FullTicketComments";
-import { FullTicketAdditionInfo } from "./components/FullTicketAdditionInfo";
-import { FullTicketFiles } from "./components/FullTicketFiles";
+import { Loader } from '../../components/Loader'
+import { ActionPanel } from './components/ActionPanel'
+import { MarkdownWithStyles } from '../../utils/markdown'
+import { FullTicketHeader } from './components/FullTicketHeader'
+import { FullTicketComments } from './components/FullTicketComments'
+import { FullTicketAdditionInfo } from './components/FullTicketAdditionInfo'
+import { FullTicketFiles } from './components/FullTicketFiles'
 
-import { useShowTicketMutation } from "api/tickets.api";
-import { checkIsAdmin } from "functions/index";
-import IPalette from "theme/IPalette.interface";
+import { useShowTicketMutation } from 'api/tickets.api'
+import { checkIsAdmin } from 'functions/index'
+import IPalette from 'theme/IPalette.interface'
 import {
   useAdminShowTicketMutation,
   useAdminUpdateTicketMutation,
-} from "api/admin.api";
-import { getUser } from "functions/manipulateLocalStorage";
+} from 'api/admin.api'
+import { getUser } from 'functions/manipulateLocalStorage'
 import {
   useToggleBookmarkMutation,
   useToggleLikeMutation,
-} from "api/tickets.api";
-import { useCommentsConnection } from "./hooks/useCommentsConnection";
-import { useToggleAction } from "hooks/index";
-import { toggleOptions } from "constants";
+} from 'api/tickets.api'
+import { useCommentsConnection } from './hooks/useCommentsConnection'
+import { useToggleAction } from 'hooks/index'
+import { toggleOptions } from 'constants'
 
 export interface IPerson {
-  color: string;
-  nick: string;
+  color: string
+  nick: string
 }
 
 const FullTicketInfo: FC = () => {
-  const { palette }: IPalette = useTheme();
-  const { pathname } = useLocation();
+  const { palette }: IPalette = useTheme()
+  const { pathname } = useLocation()
 
-  const ticketId = parseInt(pathname.split("/")[2]);
-  const commentsConnection = useCommentsConnection(ticketId);
-  const isAdmin = checkIsAdmin();
-  const { userId } = getUser();
+  const ticketId = parseInt(pathname.split('/')[2])
+  const commentsConnection = useCommentsConnection(ticketId)
+  const isAdmin = checkIsAdmin()
+  const { userId } = getUser()
 
   const [peopleSettings, setPeopleSettings] = useState(
     new Map<number, IPerson>()
-  );
-  const [upvotes, setUpvotes] = useState<number>(0);
-  const [isLiked, setIsLiked] = useState<boolean>(false);
-  const [isFollowed, setIsFollowed] = useState<boolean>(false);
+  )
+  const [upvotes, setUpvotes] = useState<number>(0)
+  const [isLiked, setIsLiked] = useState<boolean>(false)
+  const [isFollowed, setIsFollowed] = useState<boolean>(false)
 
   const [showTicket, { data: ticket, isSuccess, isLoading }] = isAdmin
     ? useAdminShowTicketMutation()
-    : useShowTicketMutation();
+    : useShowTicketMutation()
   const [updateTicket, { isSuccess: isSuccessUpdate }] =
-    useAdminUpdateTicketMutation();
-  const [toggleLike] = useToggleLikeMutation();
-  const [toggleFollowed] = useToggleBookmarkMutation();
+    useAdminUpdateTicketMutation()
+  const [toggleLike] = useToggleLikeMutation()
+  const [toggleFollowed] = useToggleBookmarkMutation()
 
-  const isMyTicket = userId == ticket?.creator.user_id;
-  const isCanManipulateFile = isMyTicket || isAdmin;
+  const isMyTicket = userId == ticket?.creator?.user_id
+  const isCanManipulateFile = isMyTicket || isAdmin
 
   useEffect(() => {
-    showTicket({ body: JSON.stringify({ ticket_id: ticketId }) });
-  }, [ticketId, isSuccessUpdate]);
+    showTicket({ body: JSON.stringify({ ticket_id: ticketId }) })
+  }, [ticketId, isSuccessUpdate])
 
   useEffect(() => {
     if (isSuccess) {
-      setIsLiked(ticket?.is_liked);
-      setUpvotes(ticket?.upvotes);
-      setIsFollowed(ticket?.is_followed);
+      setIsLiked(ticket?.is_liked)
+      setUpvotes(ticket?.upvotes)
+      setIsFollowed(ticket?.is_followed)
     }
-  }, [isSuccess]);
+  }, [isSuccess])
 
-  const likeOption = !isLiked ? toggleOptions.LIKE : toggleOptions.UNLIKE;
+  const likeOption = !isLiked ? toggleOptions.LIKE : toggleOptions.UNLIKE
 
   const likeOptions = {
     toggleMutation: toggleLike,
@@ -82,15 +82,15 @@ const FullTicketInfo: FC = () => {
     dependencies: [isLiked],
     callback: () => {
       setUpvotes((prevUpvote: number) =>
-        likeOption === "like" ? prevUpvote + 1 : prevUpvote - 1
-      );
+        likeOption === 'like' ? prevUpvote + 1 : prevUpvote - 1
+      )
     },
-  };
-  const handleToggleLike = useToggleAction(likeOptions);
+  }
+  const handleToggleLike = useToggleAction(likeOptions)
 
   const followedOption = !isFollowed
     ? toggleOptions.BOOKMARK
-    : toggleOptions.UNBOOKMARK;
+    : toggleOptions.UNBOOKMARK
 
   const followedOptions = {
     toggleMutation: toggleFollowed,
@@ -98,8 +98,8 @@ const FullTicketInfo: FC = () => {
     setState: setIsFollowed,
     ticketId: ticket?.ticket_id,
     dependencies: [isFollowed],
-  };
-  const handleToggleFollowed = useToggleAction(followedOptions);
+  }
+  const handleToggleFollowed = useToggleAction(followedOptions)
 
   return (
     <Grid container>
@@ -111,7 +111,7 @@ const FullTicketInfo: FC = () => {
           sx={{
             pt: 6,
             pb: 10,
-            "& > .MuiGrid-root > .MuiTypography-root:not(:first-of-type)": {
+            '& > .MuiGrid-root > .MuiTypography-root:not(:first-of-type)': {
               fontSize: 20,
             },
           }}
@@ -130,11 +130,11 @@ const FullTicketInfo: FC = () => {
           <Grid container>
             <Grid
               sx={{
-                width: "100%",
-                p: "16px 20px",
+                width: '100%',
+                p: '16px 20px',
                 bgcolor: palette.grey.card,
                 borderRadius: 1,
-                whiteSpace: "pre-line",
+                whiteSpace: 'pre-line',
               }}
             >
               <MarkdownWithStyles innerText={ticket.body} />
@@ -145,7 +145,7 @@ const FullTicketInfo: FC = () => {
             isCanManipulateFile={isCanManipulateFile}
           />
           <FullTicketAdditionInfo
-            creator={ticket.creator}
+            creator={ticket?.creator}
             faculty={ticket.faculty}
             date={ticket.date}
           />
@@ -166,7 +166,7 @@ const FullTicketInfo: FC = () => {
         </Grid>
       )}
     </Grid>
-  );
-};
+  )
+}
 
-export { FullTicketInfo };
+export { FullTicketInfo }
--- src/pages/FullTicketInfo/components/FullTicketAdditionInfo/FullTicketAdditionInfo.tsx
+++ src/pages/FullTicketInfo/components/FullTicketAdditionInfo/FullTicketAdditionInfo.tsx
@@ -1,24 +1,24 @@
-import { FC } from "react";
-import { NavLink } from "react-router-dom";
-import { useTranslation } from "react-i18next";
+import { FC } from 'react'
+import { NavLink } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
 
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
-import useTheme from "@mui/material/styles/useTheme";
+import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+import useTheme from '@mui/material/styles/useTheme'
 
-import IPalette from "theme/IPalette.interface";
-import { formatDate } from "functions/index";
-import { endpoints } from "constants";
-import { ICreator } from "components/Ticket/components/TicketBody/TicketBody";
+import IPalette from 'theme/IPalette.interface'
+import { formatDate } from 'functions/index'
+import { endpoints } from 'constants'
+import { ICreator } from 'components/Ticket/components/TicketBody/TicketBody'
 
 interface FullTicketAdditionInfoProps {
-  creator: ICreator;
+  creator: ICreator | null
   faculty: {
-    faculty_id: number;
-    name: string;
-  };
-  date: string;
+    faculty_id: number
+    name: string
+  }
+  date: string
 }
 
 const FullTicketAdditionInfo: FC<FullTicketAdditionInfoProps> = ({
@@ -26,54 +26,54 @@ const FullTicketAdditionInfo: FC<FullTicketAdditionInfoProps> = ({
   faculty,
   date,
 }) => {
-  const { t } = useTranslation();
-  const { palette }: IPalette = useTheme();
+  const { t } = useTranslation()
+  const { palette }: IPalette = useTheme()
 
-  const formattedData = formatDate(date);
+  const formattedData = formatDate(date)
 
   return (
     <Grid container>
-      <Typography mb={2}>{t("fullTicket.additionalInfo")}</Typography>
+      <Typography mb={2}>{t('fullTicket.additionalInfo')}</Typography>
       <Grid
         container
         sx={{
           gap: 1,
-          "& > .MuiGrid-root": {
-            flexBasis: { xs: "100%", sm: "calc((100% - 16px) / 3)" },
+          '& > .MuiGrid-root': {
+            flexBasis: { xs: '100%', sm: 'calc((100% - 16px) / 3)' },
             p: 2,
             gap: 1,
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: { xs: "center", lg: "" },
-            flexDirection: { xs: "row", sm: "column", lg: "row" },
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: { xs: 'center', lg: '' },
+            flexDirection: { xs: 'row', sm: 'column', lg: 'row' },
             bgcolor: palette.grey.card,
             borderRadius: 1,
-            "& > *": {
-              color: "rgba(255, 255,255, 0.8)",
+            '& > *': {
+              color: 'rgba(255, 255,255, 0.8)',
             },
           },
         }}
       >
         <Grid>
-          <Typography>{t("fullTicket.author")}</Typography>
+          <Typography>{t('fullTicket.author')}</Typography>
           <NavLink
-            to={!creator ? "" : `${endpoints.PROFILE}/${creator.user_id}`}
+            to={!creator ? '' : `${endpoints.PROFILE}/${creator.user_id}`}
             style={{ color: palette.semantic.info }}
           >
-            @{!creator ? "anonymous" : creator.login}
+            @{!creator ? 'anonymous' : creator.login}
           </NavLink>
         </Grid>
         <Grid>
-          <Typography>{t("fullTicket.faculty")}</Typography>
+          <Typography>{t('fullTicket.faculty')}</Typography>
           <Box>{faculty.name}</Box>
         </Grid>
         <Grid>
-          <Typography>{t("fullTicket.dateOfCreation")}</Typography>
+          <Typography>{t('fullTicket.dateOfCreation')}</Typography>
           <Box>{formattedData}</Box>
         </Grid>
       </Grid>
     </Grid>
-  );
-};
+  )
+}
 
-export { FullTicketAdditionInfo };
+export { FullTicketAdditionInfo }
