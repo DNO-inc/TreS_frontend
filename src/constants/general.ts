20ee5a3212d9986f8199418c1d102c87ec8deb47
yaroslav <usenko.0265@gmail.com>
2024-05-07 14:07:15 +0300

    Added cabinet menu

--- src/constants/general.ts
+++ src/constants/general.ts
@@ -1,9 +1,9 @@
 const general = {
-  TRES_TOKEN: "oc62XD7C",
+  TRES_TOKEN: 'q87nfWyQ',
   MAX_TITLE_LENGTH: 50,
   MAX_BODY_LENGTH: 5000,
   CABINET_POPUP_WIDTH: 600,
   CABINET_POPUP_HEIGHT: 600,
-};
+}
 
-export { general };
+export { general }
--- src/layouts/MainLayout/components/Header/Header.tsx
+++ src/layouts/MainLayout/components/Header/Header.tsx
@@ -1,89 +1,89 @@
-import { FC } from "react";
-import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { FC } from 'react'
+import { useLocation, useNavigate, useParams } from 'react-router-dom'
 
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import Toolbar from "@mui/material/Toolbar";
-import useTheme from "@mui/material/styles/useTheme";
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import IconButton from '@mui/material/IconButton'
+import Toolbar from '@mui/material/Toolbar'
+import useTheme from '@mui/material/styles/useTheme'
 
-import MenuIcon from "@mui/icons-material/Menu";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import MenuIcon from '@mui/icons-material/Menu'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack'
 
-import { AuthZone } from "./components/AuthZone";
+import { AuthZone } from './components/AuthZone'
 
-import { dimensions, endpoints } from "constants";
-import IPalette from "theme/IPalette.interface";
+import { dimensions, endpoints } from 'constants'
+import IPalette from 'theme/IPalette.interface'
 
 interface HeaderProps {
-  handleDrawerToggle: () => void;
+  handleDrawerToggle: () => void
 }
 
 const Header: FC<HeaderProps> = ({ handleDrawerToggle }) => {
-  const { palette }: IPalette = useTheme();
-  const { pathname } = useLocation();
-  const navigate = useNavigate();
-  const { ticketId } = useParams();
+  const { palette }: IPalette = useTheme()
+  const { pathname } = useLocation()
+  const navigate = useNavigate()
+  const { ticketId } = useParams()
 
-  const drawerWidth = dimensions.DRAWER_WIDTH;
+  const drawerWidth = dimensions.DRAWER_WIDTH
 
-  const isFullTicketInfo: boolean = !!ticketId;
-  const isCreateTicket: boolean = pathname === endpoints.CREATE_TICKET;
-  const isDrawStepBackAction: boolean = isFullTicketInfo || isCreateTicket;
+  const isFullTicketInfo: boolean = !!ticketId
+  const isCreateTicket: boolean = pathname === endpoints.CREATE_TICKET
+  const isDrawStepBackAction: boolean = isFullTicketInfo || isCreateTicket
 
   const handleClick = (): void => {
-    navigate(-1);
-  };
+    navigate(-1)
+  }
 
   return (
     <AppBar
-      position="fixed"
+      position='fixed'
       sx={{
         width: { md: `calc(100% - ${drawerWidth}px)` },
         ml: { md: `${drawerWidth}px` },
-        boxShadow: "none",
-        backgroundImage: "none",
+        boxShadow: 'none',
+        backgroundImage: 'none',
         backgroundColor: palette.grey.background,
         borderBottom: `2px solid ${palette.grey.card}`,
       }}
     >
       <Toolbar
         sx={{
-          display: "flex",
-          height: "64px",
+          display: 'flex',
+          height: '64px',
           justifyContent: {
-            xs: "space-between",
-            md: "flex-end",
+            xs: 'space-between',
+            md: 'flex-end',
           },
-          alignItems: "center",
+          alignItems: 'center',
         }}
       >
         <IconButton
-          color="inherit"
-          aria-label="open drawer"
-          edge="start"
+          color='inherit'
+          aria-label='open drawer'
+          edge='start'
           onClick={handleDrawerToggle}
-          sx={{ ml: -1, display: { xs: "inline-flex", md: "none" } }}
+          sx={{ ml: -1, display: { xs: 'inline-flex', md: 'none' } }}
         >
           <MenuIcon />
         </IconButton>
         {isDrawStepBackAction && (
           <Box
             sx={{
-              position: "absolute",
+              position: 'absolute',
               top: 0,
               left: 0,
               bottom: 0,
               width: 48,
               bgcolor: palette.grey.card,
-              display: { xs: "none", md: "block" },
+              display: { xs: 'none', md: 'block' },
             }}
           >
             <Button
-              color="inherit"
+              color='inherit'
               onClick={handleClick}
-              sx={{ minWidth: "100%", minHeight: "100%" }}
+              sx={{ minWidth: '100%', minHeight: '100%' }}
             >
               <ArrowBackIcon sx={{ color: palette.common.white }} />
             </Button>
@@ -92,7 +92,7 @@ const Header: FC<HeaderProps> = ({ handleDrawerToggle }) => {
         <AuthZone />
       </Toolbar>
     </AppBar>
-  );
-};
+  )
+}
 
-export { Header };
+export { Header }
